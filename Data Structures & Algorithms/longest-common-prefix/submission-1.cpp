7b78464a950af47d85a84e0f9c912445f3ee57a6
Shafayetul Huda Sadi <shafayet.sadi@gmail.com>
2026-09-05 22:16:44 +0600

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = "";
+        int index = -1;
+        bool flag = true;
+        for (int i = 0; i < strs[0].size(); i++) { // b, a, t => O(n)
+            for (int j = 0; j < strs.size(); j++) { // "bat", "bag", "bank","band" => O(m)
+                if (i == strs[j].size() or strs[j][i] != strs[0][i]) return res;
+            }
+            res += strs[0][i]; 
+        }
+        return res;
+    }
+};
